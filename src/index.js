bc7a8847e95c9c9e181a03817b56bd2fad252c02
TanyaKemmPere <tanya.kemmpereira@heig-vd.ch>
2022-03-25 10:05:06 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import * as d3 from 'd3';
 import { json } from 'd3-fetch'
 
-
-d3.json('../donneesSpotifyParMois/data-2017-01-31')
+console.log(42)
+d3.json('../data-2017-01-31.json')
     .then( function(data)
     {
         console.log ("musique", data)
