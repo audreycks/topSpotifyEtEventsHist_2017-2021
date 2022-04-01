65cce8ce041567d928879adb603e3fb008ba9868
TanyaKemmPere <tanya.kemmpereira@heig-vd.ch>
2022-04-01 14:12:32 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,37 +3,64 @@ import { json } from 'd3-fetch'
 
 
 // set the dimensions and margins of the graph
-const height = window.innerHeight*0.8;
-const width = window.innerWidth*0.6;
+const height = 100 //window.innerHeight*0.8;
+const width = 200 //window.innerWidth*0.6;
 const margin = { top: 50, bottom: 50, left: 50, right: 50 }
 
+
+// append les titres des chansons
+let titre = d3.select(".titres")
+    .append("h2")
+
+let artiste = d3.select(".titres")
+    .append("p")
+    .text("artiste")
+    
+let album = d3.select(".titres")
+    .append("p")
+    .text("album")
+
+// append les events
+let events = d3.select(".events")
+    .append("p")
+    .text("quand blabla")
+
 // append the svg object to the body of the page
-const svg = d3.select("#spotifyAndEvents")
-            .append("svg")
-            .attr("class", "graphe")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-           // .attr('viewBox', [0, 0, width, height])
-            .attr("transform", `translate(${margin.left},${margin.top}`)
-            .append("g")
-            .attr("transform", `translate(${margin.left},${margin.top}`)
+let svgGraphe = d3.select(".stats")
+    .append("svg")
+    .attr("class", "graphe")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .attr("transform", `translate(${margin.left},${margin.top}`)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top}`)
 
 
-console.log(42);
-
-d3.json('../data-2017.json')
-    .then( function(data_2017)
+Promise.all([
+    json('../data-2017.json'),
+    json('../data-2018.json'),
+    json('../data-2019.json'),
+    json('../data-2020.json'),
+    json('../data-2021.json'),
+    json('../events.json')
+])
+//d3.json('../data-2017.json')
+    .then(([data_2017, data_2018, data_2019, data_2020, data_2021, data_events]) =>
     {
 
+
+        
+
+        /*
         // Parse the Data
 
         //axe X
         const x = d3.scaleBand()
-        .domain(data_2017.map(m => m.name))
+            .domain(data_2017.map(m => m.name))
             .range([0, width])
             .padding(0.2);
 
-        svg.append("g")
+        svgGraphe.append("g")
             .attr("transform", `translate(0, ${height})`)
             .call(d3.axisBottom(x))
           
@@ -45,13 +72,13 @@ d3.json('../data-2017.json')
             .range([ height, 0]);
 
 
-        svg.append("g")
+        svgGraphe.append("g")
             .attr("transform", `translate(${margin.left}, 0)`)
             .call(d3.axisLeft(y));
 
 
         //bars
-        svg.selectAll("rect")
+        svgGraphe.selectAll("rect")
             .data(data_2017)
             .enter()
             .append("rect")
@@ -62,10 +89,11 @@ d3.json('../data-2017.json')
             .attr("fill", "#69b3a2")
 
 
-        /*
-        data_2017.forEach(el => {
-            console.log(el.genres)
-        });*/
+
+        
+        /*data_2017.name.forEach(e => {
+            console.log(e.name)
+        }); /** */
 
         /*
         const musiques = data_2017.reduce((m, music) =>
@@ -77,16 +105,3 @@ d3.json('../data-2017.json')
     .catch(function(error){
         console.log(error);
     }) /**/
-
-
-/*
-Promise.all ([
-    json("../data_2017.json")
-    //json("../data_2018.json")
-])
-    .then(([data_2017, data_2018]) => {
-        console.log ("musique", data_2017);
-    })
-    .catch(function(error){
-        console.log(error);
-    })/**/ 
