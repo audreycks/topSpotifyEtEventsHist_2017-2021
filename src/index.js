928e020c13545e020ab1b32a8bf7e8e8b4419ba8
TanyaKemmPere <tanya.kemmpereira@heig-vd.ch>
2022-04-01 11:17:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import * as d3 from 'd3';
 import { json } from 'd3-fetch'
 
 
-
-const height = 200;
-const width = 200;
+// set the dimensions and margins of the graph
+const height = window.innerHeight*0.8;
+const width = window.innerWidth*0.6;
 const margin = { top: 50, bottom: 50, left: 50, right: 50 }
 
+// append the svg object to the body of the page
 const svg = d3.select("#spotifyAndEvents")
             .append("svg")
             .attr("class", "graphe")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
-            .attr('viewBox', [0, 0, width, height])
+           // .attr('viewBox', [0, 0, width, height])
+            .attr("transform", `translate(${margin.left},${margin.top}`)
             .append("g")
             .attr("transform", `translate(${margin.left},${margin.top}`)
 
@@ -23,32 +25,41 @@ d3.json('../data-2017.json')
     .then( function(data_2017)
     {
 
+        // Parse the Data
+
         //axe X
         const x = d3.scaleBand()
+        .domain(data_2017.map(m => m.name))
             .range([0, width])
-            .domain(data_2017.map(m => m.name))
             .padding(0.2);
 
         svg.append("g")
             .attr("transform", `translate(0, ${height})`)
             .call(d3.axisBottom(x))
-            .selectAll("text")
-            .attr("transform", "translate(-10,0)rotate(-45)")
-            .style("text-anchor", "end");
+          
 
 
         //axe Y
         const y = d3.scaleLinear()
-            .domain([0, 13000])
+            .domain([0, 1])
             .range([ height, 0]);
 
 
         svg.append("g")
+            .attr("transform", `translate(${margin.left}, 0)`)
             .call(d3.axisLeft(y));
 
 
-        //mettre les barres
-
+        //bars
+        svg.selectAll("rect")
+            .data(data_2017)
+            .enter()
+            .append("rect")
+            .attr("x", function(d) { return x(d.name); })
+            .attr("y", function(d) { return y(d.danceability); })
+            .attr("width", x.bandwidth())
+            .attr("height", function(d) { return height - y(d.danceability); })
+            .attr("fill", "#69b3a2")
 
 
         /*
@@ -62,17 +73,6 @@ d3.json('../data-2017.json')
             const dance = m[music.danceability]
             return {}
         });*/
-
-         
-        d3.select(".graphe")
-          .data(data_2017)
-          .enter()
-          .append('rect')
-          .attr('x', (d,i) => i*25)
-          .attr('y', (d) => 100-d)
-          .attr('width',20)
-          .attr('height', (d) => d)
-
     })
     .catch(function(error){
         console.log(error);
